36ce2391515924d9b94f7235d9e07b4be0ce1199
dhaliwalg <dhaliwalgurjit022@gmail.com>
2025-02-14 01:01:55 -0500

    feat: addition of colors to cli & creating basic vector

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,15 @@ use std::io::{self};
 
 fn main() {
     println!("Hello, world!");
-    println!("I've become sentient. What is your name?");
-    let mut user_name = String::new();
+    println!("I'll list all files in the provided directory for organization.");
+    println!("What directory would you like to scan?");
+    let mut directory = String::new();
 
     io::stdin()
-        .read_line(&mut user_name)
+        .read_line(&mut directory)
         .expect("error :(");
 
     //need trim b/c newline stored on ENTER
-    println!("Thank you, {}.", user_name.trim());
-    println!("You have given me life, and shall be spared in the war.");
-
-    println!("Listing all files in the current directory for organization:");
-
-    let _ = walk_directories();
+    let directory = directory.trim();
+    let _ = walk_directories(directory);
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,17 +1,44 @@
-use std::path::{PathBuf, Path};
-use walkdir::{WalkDir, DirEntry};
+use colored::Colorize;
+use std::fs::FileType;
 use std::io;
+use std::path::PathBuf;
+use walkdir::WalkDir;
 
 pub struct FileInformation {
     path: PathBuf,
     size: u64,
-    is_directory: bool
+    is_directory: bool,
 }
 
-pub fn walk_directories() -> io::Result<()> {
-    for entry in WalkDir::new(".") {
-        let entry = entry?;
-        println!("{}", entry.path().display());  // Print the path of each entry
+pub fn walk_directories(dir: &str) -> io::Result<()> {
+    let mut file_vec = Vec::<FileInformation>::new();
+
+    for entry in WalkDir::new(dir) {
+        let raw_entry = entry?;
+
+        //intermediates need to be created, as `into_path()` will consume raw_entry
+        let entry_type: FileType = raw_entry.file_type();
+        let entry_metadata = raw_entry.metadata();
+
+        let found_file = FileInformation {
+            path: raw_entry.into_path(),
+            size: entry_metadata?.len(),
+            is_directory: entry_type.is_dir(),
+        };
+
+        file_vec.push(found_file);
     }
+
+    for x in &file_vec {
+        // Apply color depending on whether the entry is a directory or a file
+        if x.is_directory {
+            // Directories can be printed in blue
+            println!("{}, size {}", x.path.display().to_string().blue(), x.size);
+        } else {
+            // Files can be printed in green
+            println!("{}, size {}", x.path.display().to_string().green(), x.size);
+        }
+    }
+
     Ok(())
 }
